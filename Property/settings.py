bc1e86821c7f608bb61533e325c731106a8197ae
vaibhav2607 <vaibhav1946vj@gmail.com>
2020-02-20 15:09:59 +0530

    /Removed email from settings/

--- Property/settings.py
+++ Property/settings.py
@@ -142,13 +142,6 @@ MESSAGE_TAG = {
 
 }
 
-# Email Config
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vaiana1426@gmail.com'
-EMAIL_HOST_PASSWORD = 'kwqcrlkomwqddvbl'
-EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
